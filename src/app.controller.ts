ef2223dd62c7996d03d3cbf7a5624e94cd5e1f54
Pradeep_Ranasinghe <pradeep@climatesi.com>
2023-07-28 15:52:13 +0530

    pipeline

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { exec } from 'child_process';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,37 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('deploy')
+  deploy(){
+    console.log("deloying the main main service ---------------------------------")
+    exec('sh /home/ubuntu/code/pmu-deploy.sh',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });       
+
+    return true;
+
+  }
+
+  @Get('deloyweb')
+  deloyweb(){
+    console.log("deloying the FE ---------------------------------")
+    exec('sh /home/ubuntu/code/pmu-web-deploy.sh',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });       
+
+    return true;
+
   }
 }
