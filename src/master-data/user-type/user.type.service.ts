53e5921db2cf80e1d713bcab9a9179cc0cadcedb
Dilanka Ekanayake <dilanka@climatesi.com>
2024-02-08 11:07:03 +0530

    usertypes

--- src/master-data/user-type/user.type.service.ts
+++ src/master-data/user-type/user.type.service.ts
@@ -5,7 +5,16 @@ import { UserType } from "src/users/user.type.entity";
 
 @Injectable()
 export class UserTypeService extends TypeOrmCrudService<UserType> {
-    constructor(@InjectRepository(UserType) repo) {
-        super(repo);
-      }
+
+  constructor(@InjectRepository(UserType) repo) {
+    super(repo);
+  }
+  async  getManyUserTypes(filter: number[]):Promise<UserType[]> {
+    let data = this.repo
+    .createQueryBuilder('userType')
+    .andWhere('userType.id NOT IN (:ids)', { ids: filter })
+    .orderBy('name', 'ASC');
+    return await data.getMany()
+
+  }
 }
